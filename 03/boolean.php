b36eced2ca40bff9c240cf7c78c39556c6ecc03a
Takapirorin <it0a.idk0i.i0yo0ni@gmail.com>
2023-01-24 18:57:44 +0900

    add:05 di

---
+++ 03/boolean.php
@@ -0,0 +1,14 @@
+<?php
+// var_dump((bool) 1);t
+// var_dump((bool) -2);t
+// var_dump((bool) 0);f
+// var_dump((bool) null);f
+// var_dump((bool) 'foo');t
+// var_dump((bool) false);f
+// var_dump((bool) 'false');t
+// var_dump((bool) '');f
+// var_dump((bool) 0.0);f
+// var_dump((bool) [1]);t
+// var_dump((bool) undefined);e
+// var_dump((bool) []);f
+// var_dump((bool) '0');f

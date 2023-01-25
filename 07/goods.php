3b48fe98ba9645eb9e8812ac85ef6f21d8cdc946
Takapirorin <it0a.idk0i.i0yo0ni@gmail.com>
2023-01-25 17:43:45 +0900

    add:04

--- 07/goods.php
+++ 07/goods.php
@@ -1,11 +1,22 @@
 <?php
 
-$id = $_GET['id'];
 $goodsList = ['テレビ','パソコン','携帯電話','冷蔵庫','洗濯機'];
+$id = $_GET['id'];
 $itemName = $goodsList[$id];
 
 ?>
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品の選択</title>
+</head>
+<body>
+    
+</body>
+</html>
 <p><?=$itemName?>が選択されました</p>
 
 <p><a href="lists.php">一覧ページに戻る</a></p>

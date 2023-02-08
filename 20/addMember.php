fcda10b105f27c23efe0fb7d0dea6da904b1c011
Takapirorin <it0a.idk0i.i0yo0ni@gmail.com>
2023-02-08 14:18:44 +0900

    fix:20 addMember

--- 20/addMember.php
+++ 20/addMember.php
@@ -4,26 +4,25 @@ declare(strict_types=1);
 require_once(dirname(__FILE__) . '/db.inc.php');
 require_once(dirname(__FILE__) . '/util.inc.php');
 
-$name = '';
-$age = '';
-$address = '';
-$created_at = date('Y-m-d H:i:s');
+$name       = '';
+$age        = '';
+$address    = '';
 if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $age = $_POST['age'];
+    $name    = $_POST['name'];
+    $age     = $_POST['age'];
     $address = $_POST['address'];
 
     try {
         $pdo = dbConnect();
         $stmt = $pdo->prepare(
-            'INSERT INTO members (name, age, address, created_at) VALUES (:name, :age, :address, :created_at)'
+            'INSERT INTO members (name, age, address, created_at) VALUES (:name, :age, :address, NOW())'
         );
-        $stmt->bindValue(':name', (string)$name, PDO::PARAM_STR);
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
         $stmt->bindValue(':age', (int)$age, PDO::PARAM_INT);
-        $stmt->bindValue(':address', (string)$address, PDO::PARAM_STR);
-        $stmt->bindValue(':created_at', (string)$created_at, PDO::PARAM_STR);
+        $stmt->bindValue(':address', $address, PDO::PARAM_STR);
         $stmt->execute();
-        header('Location: /practice/04_PHP/20/member.php');
+        header('Location: ./member.php');
+        exit;
     } catch (PDOException $e) {
         header('Content-Type: text/plain; charset=UTF-8', true, 500);
         exit($e->getMessage());
@@ -46,9 +45,9 @@ if (!empty($_POST)) {
     <h1>会員登録</h1>
     <p><a href="member.php">会員一覧に戻る</a></p>
     <form action="" method="post" novalidate>
-        <p>氏名：<input type="text" name="name" value="<?=h($name)?>"></p>
-        <p>年齢：<input type="text" name="age" value="<?=h($age)?>"></p>
-        <p>住所：<input type="text" name="address" value="<?=h($address)?>"></p>
+        <p>氏名：<input type="text" name="name" value="<?= h($name) ?>"></p>
+        <p>年齢：<input type="text" name="age" value="<?= h($age) ?>"></p>
+        <p>住所：<input type="text" name="address" value="<?= h($address) ?>"></p>
         <p><input type="submit" value="送信"></p>
     </form>
 </body>
